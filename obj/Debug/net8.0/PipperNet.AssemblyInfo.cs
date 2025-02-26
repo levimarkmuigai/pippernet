932b790f8512362c654378ef0a8111278e5624e4
levimarkmuigai <levimarkmuigai@gmail.com>
2025-02-26 14:07:47 +0300

    NewMigrations

--- obj/Debug/net8.0/PipperNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/PipperNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PipperNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6e3b620cc8da52e0b8c1829842e80c14685aa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+253afc42377f6b9b248f45be5adc0a1e46286394")]
 [assembly: System.Reflection.AssemblyProductAttribute("PipperNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PipperNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
