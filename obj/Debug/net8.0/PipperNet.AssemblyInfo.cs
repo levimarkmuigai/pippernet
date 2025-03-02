858f4b01a04c51fa98073cb57ec1fcc2b18c9efb
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-02 17:35:04 +0300

    SiteCSSupgrade

--- obj/Debug/net8.0/PipperNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/PipperNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PipperNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0dfbd8f73aa9cb6bfa2d575f703bc3f2d091628")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c8d4febab51a57b3eabafdce902f1887eee8ef0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PipperNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PipperNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
