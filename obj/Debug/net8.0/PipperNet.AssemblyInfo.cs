819a2e84f37e8a7321d05ff001c6da5a938a6fe3
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-08 14:56:48 +0300

    DuraionControllers

--- obj/Debug/net8.0/PipperNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/PipperNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PipperNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd8196c46da7dbef7555f79f26f8668b99d95c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+366c2abb5fc1ff8f5dfedf4cb0e769e1982ded72")]
 [assembly: System.Reflection.AssemblyProductAttribute("PipperNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PipperNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
